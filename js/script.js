79346f10d27c0fb2f9e2058e25c36af6bd2c2a58
ElizavetaRogacheva <liza-97-97-97@mail.ru>
2018-04-14 20:49:58 +0300

    created script

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var JenniferButton = document.querySelector(".info-button-jennifer");
+var ModalJennifer = document.querySelector(".modal-jennifer");
+var ButtonJenniferClose = document.querySelector(".modal-jennifer-close");
+var CharlyButton = document.querySelector(".info-button-charly");
+var ModalCharly = document.querySelector(".modal-charly");
+var ButtonCharlyClose = document.querySelector(".modal-charly-close");
+
+JenniferButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    ModalJennifer.classList.add("modal-show");
+
+})
+
+ButtonJenniferClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    ModalJennifer.classList.remove("modal-show");
+    
+})
+
+CharlyButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    ModalCharly.classList.add("modal-show");
+
+})
+
+ButtonCharlyClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    ModalCharly.classList.remove("modal-show");
+
+})
